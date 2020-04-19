2ce23c8c94caf96e4336c271604c250a38a615c7
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-19 21:19:48 +0200

    Gets player controller working for now

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,18 +5,26 @@ public class PlayerController : MonoBehaviour
     //Public vars
     public float horizontalSpeed;
     public float jumpForce;
+    public bool isGrounded = false;
 
     //Player components
     private Rigidbody2D playerRigidbody;
+    private SpriteRenderer playerSpriteRenderer;
     private void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        transform.Translate(new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime, 0));
-        if (Input.GetKeyDown(KeyCode.Space))
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        //Flip Sprite depending on orientation
+        playerSpriteRenderer.flipX = horizontalAxis < 0;
+
+        transform.Translate(new Vector2(horizontalAxis * horizontalSpeed * Time.deltaTime, 0));
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }

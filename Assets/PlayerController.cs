30b084e9fe03bc613ab35a9e7a5c8ae698666b4a
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-19 21:01:17 +0200

    Adds first version of player controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Public vars
+    public float horizontalSpeed;
+    public float jumpForce;
+
+    //Player components
+    private Rigidbody2D playerRigidbody;
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime, 0));
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+}

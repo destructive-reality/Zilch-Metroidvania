335e93f605f094ad5f7016b5090e950af500ad35
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-27 23:53:09 +0200

    add levelloader script

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    //Singleton Stuff
+    private static LevelLoader _instance;
+    public static LevelLoader Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    void Update()
+    {
+        //TODO
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}

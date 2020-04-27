7f5035afd33ca46930c12bcdae55aecfc8c05643
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-28 00:04:19 +0200

    Require collider2d for level connector

--- Assets/LevelConnector.cs
+++ Assets/LevelConnector.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class LevelConnector : MonoBehaviour
 {
     public int sceneToLoad;
+
+    private void Awake()
+    {
+        //Safety valve to always set the collider to trigger
+        GetComponent<Collider2D>().isTrigger = true;
+    }
 }

7df0910b9bd9ee9092ab44e3de67731ec32db7f8
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-27 23:57:34 +0200

    Add new api method for level loader

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -36,10 +36,15 @@ public class LevelLoader : MonoBehaviour
 
     public void LoadNextLevel()
     {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        LoadLevelWithID(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    IEnumerator LoadLevel(int levelIndex)
+    public void LoadLevelWithID(int levelIndex)
+    {
+        StartCoroutine(LoadLevel(levelIndex));
+    }
+
+    private IEnumerator LoadLevel(int levelIndex)
     {
         transition.SetTrigger("Start");
         yield return new WaitForSeconds(transitionTime);

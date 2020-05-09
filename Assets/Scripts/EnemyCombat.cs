fe037e157059bc9aec33c9413feee4775701c65c
Markus <markus.damm@hs-furtwangen.de>
2020-05-09 17:40:18 +0200

    enemy can follow and attack player

--- Assets/Scripts/EnemyCombat.cs
+++ Assets/Scripts/EnemyCombat.cs
@@ -7,18 +7,31 @@ public class EnemyCombat : MonoBehaviour
     public Animator animator;
 
     public int maxHealth = 100;
-    private int currentHealth;
+    [SerializeField] private int currentHealth;
+
+    public int attackPower = 30;
+
+    public Transform player;
+
+
+    private Color firstColor;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        firstColor = GetComponent<MeshRenderer>().material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // for disctance between Monster and player
+        var playerHeading = player.position - this.transform.position;
+        var playerDistance = playerHeading.magnitude;
+        float playerAngle = Vector3.Angle(this.transform.forward, playerHeading);
+
+        transform.Translate(playerHeading * 0.1f * Time.deltaTime, Space.World);
     }
 
     public void getHit(int damage)
@@ -27,10 +40,24 @@ public class EnemyCombat : MonoBehaviour
         currentHealth -= damage;
         // Animation goes here
         // animator.SetTrigger("Hurt");
+        flashRed();
         if (currentHealth <= 0)
         {
             Die();
         }
+    }
+
+    void flashRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        Invoke("setbackColor", 0.3f);
+        // yield return new WaitForSeconds(0.3f);   // didnt work in IEnuerator
+        // GetComponent<MeshRenderer>().material.color = firstColor;
+    }
+
+    void setbackColor()
+    {
+        GetComponent<MeshRenderer>().material.color = firstColor;
     }
 
     void Die()
@@ -41,4 +68,13 @@ public class EnemyCombat : MonoBehaviour
         GetComponent<MeshRenderer>().enabled = false;
         this.enabled = false;
     }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if (collider.tag != "Player")
+            return;
+        collider.GetComponent<PlayerCombat2D>().getHit(attackPower);
+        Debug.Log("Hitting " + collider.tag + " for " + attackPower);
+    }
+
 }

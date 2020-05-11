863741d92b3d9a624e1deb4588eef9e5718686a1
Markus <markus.damm@hs-furtwangen.de>
2020-05-11 11:41:22 +0200

    Create Patrol Script

---
+++ Assets/Scripts/NonPlayer/patrol.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol : MonoBehaviour
+{
+
+    public float speed = 3f;
+    public float rayDistance = 1f;
+    public LayerMask ignoreLayers;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, rayDistance, ~ignoreLayers);
+        Debug.Log(groundInfo.collider);
+        if (groundInfo.collider == null)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+}

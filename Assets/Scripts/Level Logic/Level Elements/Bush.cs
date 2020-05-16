12899af91419a991b6b6a566027d8ad2dd270763
Bastian Hodapp <bastianhodapp@gmail.com>
2020-05-16 20:32:18 +0200

    Add Bush with sound efect and particles (first version)
    METVAN-48

---
+++ Assets/Scripts/Level Logic/Level Elements/Bush.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//Required Components
+[RequireComponent(typeof(AudioSource))]
+public class Bush : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioSource bushAudioSource;
+    public List<AudioClip> bushWalkThroughAudioClips;
+
+    [Header("Particles")]
+    public ParticleSystem bushWalkThroughParticleSystem;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            PlayBushParticlesAndSound();
+        }
+    }
+
+    void PlayBushParticlesAndSound() {
+        bushAudioSource.PlayOneShot(bushWalkThroughAudioClips[Random.Range (0, bushWalkThroughAudioClips.Count)]);
+        bushWalkThroughParticleSystem.Play();
+    }
+}

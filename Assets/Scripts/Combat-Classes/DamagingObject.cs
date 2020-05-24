b6f7975e2a272fd1dcd2337253f10db784ada969
Markus <markus.damm@hs-furtwangen.de>
2020-05-24 10:38:07 +0200

    Use static DealDamage-function
    Also problems with the constructor...

--- Assets/Scripts/Combat-Classes/DamagingObject.cs
+++ Assets/Scripts/Combat-Classes/DamagingObject.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class DamagingObject : Damaging
 {
-    // public int attackPower = 10; Error! See Comment Row 13   MD
+    // public DamagingObject()
+    // {
+    //     attackPower = 10;        // Constructor doesn't aplly those changes correctly    MD
+    //     attackRange = 0.87f;
+    // }
     private CircleCollider2D damageCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        attackPower = 10;       // How to give attackPower in code a value and show it in Unity Editor? Problem due to Inheritance     MD
+        attackPower = 10;           // Not nice, but works since the constructor doesn't    MD
         attackRange = 0.87f;
         damageCollider = GetComponent<CircleCollider2D>();
         damageCollider.radius = attackRange;
+        Debug.Log(attackRange);
     }
     protected void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag != "Player")
             return;
-        collider.GetComponent<PlayerHealth>().getHit(attackPower, gameObject);
+        Damaging.DealDamage(gameObject, attackPower, collider.gameObject);
+        // collider.GetComponent<PlayerHealth>().getHit(attackPower, gameObject);
     }
-    
+
     // draw the Radius on the object
     private void OnDrawGizmosSelected()
     {

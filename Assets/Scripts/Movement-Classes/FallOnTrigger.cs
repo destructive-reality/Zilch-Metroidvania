b88f00391f854cb9be03bf75fa9c0880d1ba8ad0
Markus <markus.damm@hs-furtwangen.de>
2020-05-24 10:39:20 +0200

    Add Fall on Trigger-script
    for damaging falling objects. It uses the static DealDamage-function

---
+++ Assets/Scripts/Movement-Classes/FallOnTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallOnTrigger : MonoBehaviour
+{
+    public int damage;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            rb.isKinematic = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player" && rb.velocity.y < 0)
+        {
+            Damaging.DealDamage(gameObject, damage, other.gameObject);
+            // other.gameObject.GetComponent<PlayerHealth>().getHit(10, gameObject);
+        }
+    }
+}

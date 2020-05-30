12604de3dbd0b38f69e9c37546662394f59b02cf
Markus <markus.damm@hs-furtwangen.de>
2020-05-30 19:16:42 +0200

    Create scripts for modifiers

---
+++ Assets/Scripts/Items/ModifierObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Modifier", menuName = "Assets/Modifier")]
+public class ModifierObject : ScriptableObject
+{
+    public string modifierName = "New Modifier";
+    public EffectObject effectBody = null;
+    public EffectObject effectArm = null;
+    public EffectObject effectLeg = null;
+    
+    [TextArea(5, 15)] 
+    public string modifierDescription = "Modifier description, also the place for some lore.";
+
+}

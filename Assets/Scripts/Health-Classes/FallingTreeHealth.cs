77bbc8ea865ae0a26c22388e76cf94871bd1a5bd
Markus <markus.damm@hs-furtwangen.de>
2020-06-01 19:14:25 +0200

    Add Script for falling tree

---
+++ Assets/Scripts/Health-Classes/FallingTreeHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTreeHealth : Health
+{
+
+    Rigidbody2D rb;
+    Vector2 hitForce = Vector2.right * 1500;
+    public Transform hitPoint;
+    private void Awake() {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    protected override void onHit()
+    {
+        if (true)
+        {
+            Debug.Log("Tree got hit!");
+            rb.AddForceAtPosition(hitForce, hitPoint.position);
+        }
+    }
+}

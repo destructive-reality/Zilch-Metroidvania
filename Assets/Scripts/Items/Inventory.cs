58c56d11d3b48431fda24777d3518505c35b6537
Markus <markus.damm@hs-furtwangen.de>
2020-06-08 18:15:12 +0200

    extend Inventory Script

--- Assets/Scripts/Items/Inventory.cs
+++ Assets/Scripts/Items/Inventory.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public List<ModifierObject> modifiers;
-    
-    public void AddModifier(ModifierObject modifier) {
+
+    private void Update()
+    {
+        if (modifiers.Count > 0)
+        {
+            foreach (ModifierObject modifier in modifiers)
+            {
+                if (modifier.effectBody.isUpdate)
+                {
+                    // modifier.effectBody.effectScript.Update();
+                }
+            }
+        }
+    }
+    public void AddModifier(ModifierObject modifier)
+    {
         modifiers.Add(modifier);
         Debug.Log(modifier.modifierName + " added to Player Inventory");
+        if(modifier.effectBody.isStart)
+        {
+            Invoke("Effects." + modifier.effectBody.effectName + "Start", 0);
+            // modifier.effectBody.effectScript.Start();
+        }
     }
 }

c135f737f69b5ec9dccfb10588a22fa1c3ca585c
Markus <markus.damm@hs-furtwangen.de>
2020-06-08 18:14:16 +0200

    Create EfectScripts-folder and first script

---
+++ Assets/Scripts/Items/EffectScripts/Effects.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effects : MonoBehaviour
+{
+    public void DefensePowerStart(GameObject player)
+    {
+        Debug.Log("Increase Player Health");
+        player.GetComponent<PlayerHealth>().maxHealth += 10;
+    }
+}

05dc5d31200acfe483a23fcb3832fd0015438b95
Markus <markus.damm@hs-furtwangen.de>
2020-06-09 07:42:38 +0200

    Create PowerEffect for first Modifier as child of Effect

---
+++ Assets/Scripts/Items/EffectScripts/PowerEffect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PowerEffect : Effect
+{
+
+    public override void ArmStart()
+    {
+
+    }
+    public override void LegStart()
+    {
+
+    }
+    public override void BodyStart()
+    {
+        Debug.Log("Increase Player Health");
+        PlayerHealth playerHealth = gameObject.GetComponentInParent<PlayerHealth>();
+        playerHealth.maxHealth += 10;
+        playerHealth.setHealth(playerHealth.maxHealth);
+        // player.GetComponent<PlayerHealth>().maxHealth += 10;
+    }
+    public override void HeadStart()
+    {
+
+    }
+    public override void ArmUpdate()
+    {
+
+    }
+    public override void LegUpdate()
+    {
+
+    }
+    public override void BodyUpdate()
+    {
+
+    }
+    public override void HeadUpdate()
+    {
+
+    }
+}

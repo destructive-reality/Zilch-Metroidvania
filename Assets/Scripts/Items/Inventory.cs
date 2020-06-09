df2f29d1da3c04a00d9a5b48863d1bdac92b01fc
Markus <markus.damm@hs-furtwangen.de>
2020-06-09 07:47:41 +0200

    Adjust Inventory

--- Assets/Scripts/Items/Inventory.cs
+++ Assets/Scripts/Items/Inventory.cs
@@ -1,32 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public List<ModifierObject> modifiers;
+    public List<GameObject> items;
+    private Equipment equipment;
 
+    private void Awake() {
+        equipment = gameObject.GetComponentInParent<Equipment>();
+    }
+
+    public void AddItem(GameObject item)
+    {
+        items.Add(item);
+        Debug.Log(item.name + " added to Player Inventory");
+    }
+    public void UseItem(GameObject item)
+    {
+        Debug.Log("Using " + item.name);
+        if (item.GetComponent<Effect>())
+        {
+            equipment.EquipModifier(item, EquipmentSlot.Body);
+            items.Remove(item);
+        }
+    }
+    public void UseAll()
+    {
+        foreach (GameObject item in items)
+        {
+            UseItem(item);
+            // Könnte einen Fehler werfen, da in UseItem ein item der Liste gelöscht werden könnte. What to do..? MD
+        }
+    }
     private void Update()
     {
-        if (modifiers.Count > 0)
+        if (Input.GetKeyDown(KeyCode.I))    // For Testing  MD
         {
-            foreach (ModifierObject modifier in modifiers)
-            {
-                if (modifier.effectBody.isUpdate)
-                {
-                    // modifier.effectBody.effectScript.Update();
-                }
-            }
+            UseAll();
         }
     }
-    public void AddModifier(ModifierObject modifier)
-    {
-        modifiers.Add(modifier);
-        Debug.Log(modifier.modifierName + " added to Player Inventory");
-        if(modifier.effectBody.isStart)
-        {
-            Invoke("Effects." + modifier.effectBody.effectName + "Start", 0);
-            // modifier.effectBody.effectScript.Start();
-        }
-    }
+
+    // if(modifier.effectBody.isStart)
+    // {
+    //     Invoke("Effects." + modifier.effectBody.effectName + "Start", 0);
+    //     // modifier.effectBody.effectScript.Start();
+    // }
+    // protected OnEquipmentChange(EventArgs arguments) {
+
+    // }
+
 }

627d1edd5af3977daac1015f0fbf38edb9315c25
MarkusDamm <markus.damm@hs-furtwangen.de>
2020-06-15 22:15:02 +0200

    Add new Effect Scripts

---
+++ Assets/Scripts/Items/EffectScripts/AgileEffect.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class AgileEffect : Effect
+{
+    PlayerMovement playerMovement;
+    private bool isDoublejumped;
+    public override void ArmStart()
+    {
+        Debug.Log("Increase Player Attack Speed");
+        PlayerCombat2D playerCombat = gameObject.GetComponentInParent<PlayerCombat2D>();
+        playerCombat.attackTime -= 0.1f;
+    }
+    public override void LegStart()
+    {
+        Debug.Log("Give Player Double Jump");
+        playerMovement = gameObject.GetComponentInParent<PlayerMovement>();
+        isDoublejumped = false;
+    }
+    public override void BodyStart()   
+    {
+        Debug.Log("Increase Player Invincible Time");
+        PlayerHealth playerHealth = gameObject.GetComponentInParent<PlayerHealth>();
+        playerHealth.startInvincibleTime += 1;
+    }
+    public override void HeadStart()
+    {
+
+    }
+    public override void ArmUpdate()
+    {
+
+    }
+    public override void LegUpdate()
+    {
+        Debug.Log("Check for double Jump");
+        if (playerMovement.isAirborne() && Input.GetButtonDown("Jump") && !isDoublejumped)
+        {
+            Debug.Log("Execute Double Jump");
+            playerMovement.ApplyJumpForce();
+        }
+        if (isDoublejumped && !playerMovement.isAirborne())
+        {
+            isDoublejumped = !isDoublejumped;
+        }
+    }
+    public override void BodyUpdate()
+    {
+
+    }
+    public override void HeadUpdate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Items/EffectScripts/RangeEffect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class RangeEffect : Effect
+{
+    public override void ArmStart()
+    {
+        Debug.Log("Increase Player Attack Range");
+        PlayerCombat2D playerCombat = gameObject.GetComponentInParent<PlayerCombat2D>();
+        playerCombat.attackRange += 1;
+    }
+    public override void LegStart()
+    {
+        Debug.Log("Increase Player Dash Time");
+        PlayerMovement playerMovement = gameObject.GetComponentInParent<PlayerMovement>();
+        playerMovement.startDashTime += 0.5f;
+    }
+    public override void BodyStart()   
+    {
+        Debug.Log("Increase Player Invincible Time");
+        PlayerHealth playerHealth = gameObject.GetComponentInParent<PlayerHealth>();
+        playerHealth.startInvincibleTime += 1;
+    }
+    public override void HeadStart()
+    {
+
+    }
+    public override void ArmUpdate()
+    {
+
+    }
+    public override void LegUpdate()
+    {
+
+    }
+    public override void BodyUpdate()
+    {
+
+    }
+    public override void HeadUpdate()
+    {
+
+    }
+}

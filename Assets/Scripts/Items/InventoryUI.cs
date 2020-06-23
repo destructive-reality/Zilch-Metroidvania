36b5ef864a84d5a77dea41779c0f69999497605c
Markus <markus.damm@hs-furtwangen.de>
2020-06-23 14:23:23 +0200

    Extend InventoryUI for toggle Inventory and Cast Rays for testing

--- Assets/Scripts/Items/InventoryUI.cs
+++ Assets/Scripts/Items/InventoryUI.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class InventoryUI : MonoBehaviour
 {
     public Transform itemsParent;
+    public GameObject inventoryUI;
     Inventory inventory;
     InventorySlot[] slots;
+
+    Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.allCameras[0];
+
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Inventory>();
         inventory.onItemChangedCallback += UpdateUI;
 
         slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.Log("Cast Ray from " + Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.name + " was hit.");
+            }
+            else 
+                Debug.Log("No hit :(");
+        }
     }
 
     void UpdateUI()

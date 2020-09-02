253ab9e2d7d13651c323fcf83d401064485a3eb3
MarkusDamm <markus.damm@hs-furtwangen.de>
2020-09-02 14:24:13 +0200

    create behaviour scripts

---
+++ Assets/Scripts/NonPlayer/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class EnemyBehaviour : MonoBehaviour
+{
+    protected EnemyState state;
+
+    public void SetState(EnemyState _state)
+    {
+        state = _state;
+        StartCoroutine(state.Start());
+    }
+
+    public void UseUpdate()
+    {
+        StartCoroutine(state.Update());
+    }
+
+    public abstract void ResetState();
+}
---
+++ Assets/Scripts/NonPlayer/Enemies/FlyingEnemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : EnemyBehaviour
+{
+    public Stat speed;
+    public float flyAroundRange = 35f;
+    private Vector3 startPosition;
+    private Vector3 target;
+    private float targetDistance;
+    public void Start()
+    {
+        startPosition = new Vector3(transform.position.x, transform.position.y);
+        SetState(new FlyAround(this, startPosition, flyAroundRange, speed.getValue()));
+    }
+
+    public void FixedUpdate()
+    {
+        if (state is Wait) return;
+
+        if (target == new Vector3(0, 0, 0) && Vector3.Distance(startPosition, GameObject.FindGameObjectWithTag("Player").transform.position) <= 10f)
+        {
+            Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+            target = new Vector3(playerPosition.x, playerPosition.y);
+            Debug.Log("FlyingEnemy notices Target");
+        }
+
+        if (target != null && state is FlyAround)
+        {
+            targetDistance = Vector3.Distance(this.transform.position, target);
+            if (Vector3.Distance(startPosition, target) <= 12f)
+            {
+                Debug.Log("FlyingEnemy flies towards target");
+                SetState(new FlyTowards(this, GameObject.FindGameObjectWithTag("Player").transform.position, speed.getValue()));
+            }
+
+        }
+
+        if (state is FlyTowards)
+        {
+            targetDistance = Vector3.Distance(this.transform.position, target);
+            if (targetDistance <= 1)
+                SetState(new Wait(this));
+        }
+    }
+
+    public override void ResetState()
+    {
+        target = new Vector3();
+        SetState(new FlyAround(this, startPosition, flyAroundRange, speed.getValue()));
+    }
+
+}

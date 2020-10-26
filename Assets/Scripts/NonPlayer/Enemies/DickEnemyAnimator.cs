c62a265a6c4ce59b7a6caad4cef4e60ba01711b1
MarkusDamm <markus.damm@hs-furtwangen.de>
2020-10-26 17:56:03 +0100

    add PointPatrol to Dick

---
+++ Assets/Scripts/NonPlayer/Enemies/DickEnemyAnimator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DickEnemyAnimator : HorizontalMovingEnemy
+{
+
+    public Transform[] patrolPointTransforms;
+    private float aggresionRange;
+    private List<Vector2> patrolPoints;
+
+    public override void ResetState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public List<Vector2> getPatrolPoints()
+    {
+        return patrolPoints;
+    }
+
+    private void Start()
+    {
+        patrolPoints = new List<Vector2>();
+        foreach (Transform transform in patrolPointTransforms)
+        {
+            patrolPoints.Add(new Vector2(transform.position.x, transform.position.y));
+        }
+        // foreach (Transform transform in patrolPointTransforms)  // Might as well delete uneccessary GOs
+        // {
+        //     GameObject.Destroy(transform.gameObject);
+        // }
+
+    }
+
+    private void Update()
+    {
+        // Draw raycast to look for Player in facing-direction when not attacking
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+
+}

5382f317e18591723729adac9df7633ab31adb9d
MarkusDamm <markus.damm@hs-furtwangen.de>
2020-10-26 20:05:51 +0100

    create superclass for movement behaviours on platforms
    Compilerbau – Walking Outer Space

--- Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/PointsPatrolBehaviour.cs
+++ Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/PointsPatrolBehaviour.cs
@@ -1,45 +1,45 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class PointsPatrolBehaviour : StateMachineBehaviour
+public class PointsPatrolBehaviour : PlatformMovementBehaviour
 {
-    private DickEnemyAnimator enemyScript;
-    private float speed;
+    protected new DickEnemyAnimator enemyScript;
+    // private float speed;
     private List<Vector2> patrolPoints;
     private int currentTarget = 0;
-
-    #region MovementOnPlatform
-    private Transform groundDetector;
-    private float rayDistance = 1f;
-    private LayerMask moveOnLayer;
-    #endregion
+    
+    // similar to walkTowardsBehaviour; need to create SuperClass /\/\D
+    // #region MovementOnPlatform
+    // private Transform groundDetector;
+    // private float rayDistance = 1f;
+    // private LayerMask moveOnLayer;
+    // #endregion
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
+        base.OnStateEnter(animator, animatorStateInfo, layerIndex);
         enemyScript = animator.gameObject.GetComponent<DickEnemyAnimator>();
 
-        speed = enemyScript.speed.getValue();
         patrolPoints = enemyScript.getPatrolPoints();
-        groundDetector = enemyScript.groundDetector;
 
-        moveOnLayer = LayerMask.GetMask("Ground");
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetector.position, Vector2.down, rayDistance, moveOnLayer);
-        if (groundInfo.collider == null)
-        {
-            return;
-        }
-        if (enemyScript.isFacingRight)
-        {
-            animator.transform.Translate(Vector2.right * speed * Time.deltaTime);
-        }
-        else
-        {
-            animator.transform.Translate(Vector2.left * speed * Time.deltaTime);
-        }
+        base.OnStateUpdate(animator, animatorStateInfo, layerIndex);
+        // RaycastHit2D groundInfo = Physics2D.Raycast(groundDetector.position, Vector2.down, rayDistance, moveOnLayer);
+        // if (groundInfo.collider == null)
+        // {
+        //     return;
+        // }
+        // if (enemyScript.isFacingRight)
+        // {
+        //     animator.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        // }
+        // else
+        // {
+        //     animator.transform.Translate(Vector2.left * speed * Time.deltaTime);
+        // }
         if (Vector2.Distance(animator.transform.position, patrolPoints[currentTarget]) <= 1)
         {
             if (currentTarget + 1 >= patrolPoints.Count)
--- Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/WalkTowardsBehaviour.cs
+++ Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/WalkTowardsBehaviour.cs
@@ -1,44 +1,46 @@
 ﻿using UnityEngine;
 
-public class WalkTowardsBehaviour : StateMachineBehaviour
+public class WalkTowardsBehaviour : PlatformMovementBehaviour
 {
-    private float speed;
+    // private float speed;
     private Rigidbody2D rigidbody;
     private Vector2 playerPosition;
-    private HorizontalMovingEnemy enemyScript;
+    // private HorizontalMovingEnemy enemyScript;
 
-    #region MovementOnPlatform
-    private Transform groundDetector;
-    private float rayDistance = 1f;
-    private LayerMask moveOnLayer;
-    #endregion
+    // #region MovementOnPlatform
+    // private Transform groundDetector;
+    // private float rayDistance = 1f;
+    // private LayerMask moveOnLayer;
+    // #endregion
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        enemyScript = animator.GetComponent<HorizontalMovingEnemy>();
-        speed = enemyScript.speed.getValue();
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        // enemyScript = animator.GetComponent<HorizontalMovingEnemy>();
+        // speed = enemyScript.speed.getValue();
         rigidbody = animator.gameObject.GetComponent<Rigidbody2D>();
         playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position;
 
-        groundDetector = enemyScript.groundDetector;
-        moveOnLayer = LayerMask.GetMask ("Ground");
+        // groundDetector = enemyScript.groundDetector;
+        // moveOnLayer = LayerMask.GetMask ("Ground");
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        RaycastHit2D groundInfo = Physics2D.Raycast (groundDetector.position, Vector2.down, rayDistance, moveOnLayer);
-        if (groundInfo.collider == null)
-        {
-            return;
-        }
-        if (enemyScript.isFacingRight)
-        {
-            animator.transform.Translate(Vector2.right * speed * Time.deltaTime);
-        }
-        else
-        {
-            animator.transform.Translate(Vector2.left * speed * Time.deltaTime);
-        }
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+        // RaycastHit2D groundInfo = Physics2D.Raycast (groundDetector.position, Vector2.down, rayDistance, moveOnLayer);
+        // if (groundInfo.collider == null)
+        // {
+        //     return;
+        // }
+        // if (enemyScript.isFacingRight)
+        // {
+        //     animator.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        // }
+        // else
+        // {
+        //     animator.transform.Translate(Vector2.left * speed * Time.deltaTime);
+        // }
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)

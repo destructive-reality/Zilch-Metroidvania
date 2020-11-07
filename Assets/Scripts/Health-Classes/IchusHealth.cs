936230e1bc1cfcc9dfca797dd006ce0e3fcb420e
Bastian Hodapp <bastianhodapp@gmail.com>
2020-11-07 17:33:58 +0100

    Add death animation to Ichus (flying enemy)

---
+++ Assets/Scripts/Health-Classes/IchusHealth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IchusHealth : KnockbackHealth
+{
+    protected override void die()
+    {
+        base.die();
+        GetComponent<Animator>().SetTrigger("death");
+    }
+}
---
+++ Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/IchusDeath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IchusDeath : StateMachineBehaviour
+{
+    public float secondsToDespawn = 8f;
+
+    private float despawnTimer = 0f;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Make Ichus fall down
+        animator.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
+        //Disable non-necessary scripts to avoid harmful interaction with the player
+        animator.GetComponent<EnemyCombat>().enabled = false;
+        animator.GetComponent<Health>().enabled = false;
+        animator.GetComponent<EnemyMovement>().enabled = false;
+        animator.GetComponent<FlyingEnemyAnimator>().enabled = false;
+
+        //Change layer to "DeadEnemies"
+        animator.gameObject.layer = 15;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        despawnTimer += Time.deltaTime;
+        if (despawnTimer >= secondsToDespawn)
+        {
+            Destroy(animator.gameObject);
+        }
+    }
+}

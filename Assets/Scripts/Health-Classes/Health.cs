fd6f47f7843d2e31a43e053834c13d2520eaac31
MarkusDamm <markus.damm@hs-furtwangen.de>
2020-11-15 20:09:22 +0100

    let Dick react to taking damage

--- Assets/Scripts/Health-Classes/Health.cs
+++ Assets/Scripts/Health-Classes/Health.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿// using System;
+// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
@@ -16,24 +16,24 @@ public class Health : MonoBehaviour
         setHealth((int) maxHealth.getValue());
     }
 
-    public virtual void setHealth(int healthToSetTo)
+    public virtual void setHealth(int _healthToSetTo)
     {
-        if (healthToSetTo < 0)
+        if (_healthToSetTo < 0)
         {
             Debug.LogError("New health cannot be below zero.");
         }
-        if (healthToSetTo > maxHealth.getValue())
+        if (_healthToSetTo > maxHealth.getValue())
         {
             Debug.LogError("New health cannot be above max health.");
         }
         // Debug.Log("Setting health of " + gameObject.name + " to " + healthToSetTo);
-        currentHealth = healthToSetTo;
+        currentHealth = _healthToSetTo;
     }
 
-    public virtual void applyDamage(int damageToTake)
+    public virtual void applyDamage(int _damageToTake)
     {
-        Debug.Log(gameObject.name + " gets Hit for " + damageToTake);
-        setHealth(Mathf.Clamp(currentHealth - damageToTake, 0, (int) maxHealth.getValue()));
+        // Debug.Log(gameObject.name + " gets Hit for " + _damageToTake);
+        setHealth(Mathf.Clamp(currentHealth - _damageToTake, 0, (int) maxHealth.getValue()));
 
         if (isDead())
         {
@@ -41,10 +41,10 @@ public class Health : MonoBehaviour
         }
     }
 
-    public void heal(int amountToHeal)
+    public void heal(int _amountToHeal)
     {
         // Debug.Log(gameObject.name + " heals for " + amountToHeal);
-        setHealth(Mathf.Clamp(currentHealth + amountToHeal, 0, (int) maxHealth.getValue()));
+        setHealth(Mathf.Clamp(currentHealth + _amountToHeal, 0, (int) maxHealth.getValue()));
     }
 
     protected virtual void die()

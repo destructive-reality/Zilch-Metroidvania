13beb97b87d5db780368042cf6445c66d5cfefdc
Bastian Hodapp <bastianhodapp@gmail.com>
2020-11-30 14:01:48 +0100

    Fixes #328

--- Assets/Scripts/Level Logic/UI/TooltipHoverBoxController.cs
+++ Assets/Scripts/Level Logic/UI/TooltipHoverBoxController.cs
@@ -11,28 +11,54 @@ public class TooltipHoverBoxController : MonoBehaviour
     public static TooltipHoverBoxController Instance { get { return _instance; } }
 
     // Vars
-    public Vector2 offset;
+    public int pixelOffsetDistance;
 
     public GameObject tooltipContentParent;
+    public RectTransform tooltipContentParentRectTransform;
     public Text toolTipText;
 
-     private void Awake () {
-        if (_instance != null && _instance != this) {
-            Destroy (this.gameObject);
-        } else {
+    private int halfScreenWidth;
+    private int halfScreenHeight;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
             _instance = this;
         }
+
+        halfScreenWidth = Screen.width / 2;
+        halfScreenHeight = Screen.height / 2;
+
+        tooltipContentParentRectTransform = tooltipContentParent.GetComponent<RectTransform>();
     }
     void Update()
     {
-        transform.position = Input.mousePosition + (Vector3)offset;
+        if (tooltipContentParent.activeSelf)
+        {
+            Vector2 newMousePosition = Input.mousePosition;
+            Vector2 newTransformPosition = transform.position;
+
+            newTransformPosition = newMousePosition;
+
+            newTransformPosition.x += newMousePosition.x < halfScreenWidth ? pixelOffsetDistance + tooltipContentParentRectTransform.rect.width : -pixelOffsetDistance;
+            newTransformPosition.y += newMousePosition.y > halfScreenHeight ? pixelOffsetDistance - tooltipContentParentRectTransform.rect.height : -pixelOffsetDistance;
+
+            transform.position = newTransformPosition;
+        }
     }
 
-    public void setVisible(bool shouldBeVisible) {
+    public void setVisible(bool shouldBeVisible)
+    {
         tooltipContentParent.SetActive(shouldBeVisible);
     }
 
-    public void changeText(string newText) {
+    public void changeText(string newText)
+    {
         toolTipText.text = newText;
     }
 }

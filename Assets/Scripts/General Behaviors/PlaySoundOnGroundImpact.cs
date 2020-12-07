5acacd81590c8bf7cbbc62356544e315ab908aad
MarkusDamm <markus.damm@hs-furtwangen.de>
2020-12-07 20:06:57 +0100

    add playing sound on groundImpact when dying to Ichus

---
+++ Assets/Scripts/General Behaviors/PlaySoundOnGroundImpact.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnGroundImpact : StateMachineBehaviour
+{
+  public AudioClip soundToPlay;
+  public bool interruptPlayingSound = false;
+  private AudioSource audioSource;
+  private bool isSoundPlayed;
+
+  override public void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+  {
+    audioSource = animator.GetComponent<AudioSource>();
+    isSoundPlayed = false;
+  }
+  override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+  {
+    if (isSoundPlayed)
+    {
+      return;
+    }
+    RaycastHit2D hitDown = Physics2D.Raycast(animator.transform.position, Vector2.down, 1, LayerMask.GetMask("Ground"));
+    if (hitDown.collider != null)
+    {
+      if (soundToPlay)
+      {
+        audioSource.PlayOneShot(soundToPlay);
+      }
+      isSoundPlayed = true;
+    }
+  }
+
+}

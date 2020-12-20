e4b9b65013c7216249d0ac05bdcec1d989e1dc89
Markus <markus.damm@hs-furtwangen.de>
2020-12-20 14:01:54 +0100

    Add wallslide

--- Assets/Scripts/Movement-Classes/PlayerMovement.cs
+++ Assets/Scripts/Movement-Classes/PlayerMovement.cs
@@ -2,23 +2,22 @@
 using UnityEngine;
 
 //Required Components
-[RequireComponent(typeof(AudioSource))]
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MovementsBase
-{
+[RequireComponent (typeof (AudioSource))]
+[RequireComponent (typeof (Rigidbody2D))]
+public class PlayerMovement : MovementsBase {
     public PlayerMasterController playerMasterController;
 
-    [Header("Movement")]
-    [SerializeField] private Stat jumpForce;                          // Amount of force added when the player jumps.
-    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .16f;  // How much to smooth out the movement
-    [SerializeField] private LayerMask groundLayers;                          // A mask determining what is ground to the character
-    [SerializeField] private Transform groundCheckTransform;                  // A position marking where to check if the player is grounded.
+    [Header ("Movement")]
+    [SerializeField] private Stat jumpForce; // Amount of force added when the player jumps.
+    [Range (0, .3f)][SerializeField] private float movementSmoothing = .16f; // How much to smooth out the movement
+    [SerializeField] private LayerMask groundLayers; // A mask determining what is ground to the character
+    [SerializeField] private Transform groundCheckTransform; // A position marking where to check if the player is grounded.
     // public float runSpeed = 40f;                 // uses speed instead      MD
     private float horizontalAxisInput;
 
     #region Jumping
-    private bool isGrounded;                        // Whether or not the player is grounded.
-    [SerializeField] float groundCheckRadius = .25f; // Radius of the overlap circle to determine if grounded
+    private bool isGrounded; // Whether or not the player is grounded.
+    [SerializeField] float checkRadius = .25f; // Radius of the overlap circle to determine if grounded
 
     private float jumpTimeCounter = 0f;
     public float jumpTime = 0.25f;
@@ -27,176 +26,170 @@ public class PlayerMovement : MovementsBase
     private bool isJumping;
 
     #endregion
+    #region WallSlide
+    bool isTouchingWall;
+    public Transform wallCheck;
+    bool isWallSliding;
+    public Stat wallSlidingSpeed;
 
+    #endregion
     public Rigidbody2D playerRigidbody2D;
     // private bool movingRight = true;        // For determining which way the player is currently facing.
     private Vector2 currentVelocity = Vector2.zero;
 
     private SpriteRenderer spriteRenderer;
     public Animator playerAnimator;
-    enum State { Idle, Jumping, Dashing, Attacking };     // even necessarily when working with animator? MD
-    private State playerState;
+    enum State { Idle, Jumping, Dashing, Attacking } // even necessarily when working with animator? MD
+ private State playerState;
 
-    #region Dash
+ #region Dash
 
-    public Stat dashSpeed;
-    public float startDashTime = 0.1f;  // The time it takes to dash
-    private float dashTime;             // remaining time of a dash
-    public Stat startDashCooldown;
-    private float dashCooldown;         // remaining dash cooldown
-    private float dashDirecton;
+ public Stat dashSpeed;
+ public float startDashTime = 0.1f; // The time it takes to dash
+ private float dashTime; // remaining time of a dash
+ public Stat startDashCooldown;
+ private float dashCooldown; // remaining dash cooldown
+ private float dashDirecton;
 
-    #endregion
+ #endregion
 
-    [Header("Sounds")]
-    public AudioSource playerAudioSource;
-    public AudioClip jumpSound;
-    [Range(0.0f, 1.0f)]
-    public float jumpSoundVolume;
-    public AudioClip dashSound;
-    [Range(0.0f, 1.0f)]
-    public float dashSoundVolume;
-    public GroundSoundCollection defaultGroundSoundCollection;
-    [SerializeField] private GroundSoundCollection currentGroundSoundCollection;
-    [Range(0.0f, 1.0f)]
-    public float walkSoundVolume;
-    public float walkSoundRate;
-    private float walkSoundTimer;
+ [Header ("Sounds")]
+ public AudioSource playerAudioSource;
+ public AudioClip jumpSound;
+ [Range (0.0f, 1.0f)]
+ public float jumpSoundVolume;
+ public AudioClip dashSound;
+ [Range (0.0f, 1.0f)]
+ public float dashSoundVolume;
+ public GroundSoundCollection defaultGroundSoundCollection;
+ [SerializeField] private GroundSoundCollection currentGroundSoundCollection;
+ [Range (0.0f, 1.0f)]
+ public float walkSoundVolume;
+ public float walkSoundRate;
+ private float walkSoundTimer;
 
-    private void Awake()
-    {
-        currentGroundSoundCollection = defaultGroundSoundCollection;
+ private void Awake () {
+ currentGroundSoundCollection = defaultGroundSoundCollection;
 
-        dashTime = startDashTime;
-        playerState = State.Idle;
+ dashTime = startDashTime;
+ playerState = State.Idle;
 
-        walkSoundTimer = walkSoundRate;
+ walkSoundTimer = walkSoundRate;
     }
 
-    private void FixedUpdate()
-    {
-        horizontalAxisInput = Input.GetAxisRaw("Horizontal");
-        Vector2 targetVelocity = new Vector2(horizontalAxisInput * speed.getValue(), playerRigidbody2D.velocity.y);
-        playerRigidbody2D.velocity = Vector2.SmoothDamp(playerRigidbody2D.velocity, targetVelocity, ref currentVelocity, movementSmoothing);
+    private void FixedUpdate () {
+        horizontalAxisInput = Input.GetAxisRaw ("Horizontal");
+        Vector2 targetVelocity = new Vector2 (horizontalAxisInput * speed.getValue (), playerRigidbody2D.velocity.y);
+        playerRigidbody2D.velocity = Vector2.SmoothDamp (playerRigidbody2D.velocity, targetVelocity, ref currentVelocity, movementSmoothing);
     }
 
-    private void Update()
-    {
+    private void Update () {
         //Update gravity scale of the player's rigidbody based on jumping or falling
         playerRigidbody2D.gravityScale = playerRigidbody2D.velocity.y > 0 ? gravityScaleUp : gravityScaleDown;
 
-        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, groundLayers);
-        playerAnimator.SetFloat("horizontalVelocity", Mathf.Abs(horizontalAxisInput));      //Play Animations correctly
+        isGrounded = Physics2D.OverlapCircle (groundCheckTransform.position, checkRadius, groundLayers);
+        playerAnimator.SetFloat ("horizontalVelocity", Mathf.Abs (horizontalAxisInput)); //Play Animations correctly
 
         //Walking Sound
-        if (Mathf.Abs(horizontalAxisInput) > 0 && isGrounded)
-        {
-            if (walkSoundTimer <= 0)
-            {
-                playerAudioSource.PlayOneShot(currentGroundSoundCollection.walkSoundAudioClips[Random.Range(0, currentGroundSoundCollection.walkSoundAudioClips.Count)], walkSoundVolume);
+        if (Mathf.Abs (horizontalAxisInput) > 0 && isGrounded) {
+            if (walkSoundTimer <= 0) {
+                playerAudioSource.PlayOneShot (currentGroundSoundCollection.walkSoundAudioClips[Random.Range (0, currentGroundSoundCollection.walkSoundAudioClips.Count)], walkSoundVolume);
                 walkSoundTimer = walkSoundRate;
             }
             walkSoundTimer -= Time.deltaTime;
-        }
-        else if (walkSoundTimer <= 0)
-        {
+        } else if (walkSoundTimer <= 0) {
             walkSoundTimer = walkSoundRate;
         }
 
         //Jumping
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
+        if (Input.GetButtonDown ("Jump") && isGrounded) {
             isJumping = true;
             jumpTimeCounter = jumpTime;
-            ApplyJumpForce();
-            playerAudioSource.PlayOneShot(jumpSound, jumpSoundVolume);
+            ApplyJumpForce ();
+            playerAudioSource.PlayOneShot (jumpSound, jumpSoundVolume);
         }
 
-        if (Input.GetButton("Jump") && isJumping)
-        {
-            if (jumpTimeCounter > 0)
-            {
-                ApplyJumpForce();
+        if (Input.GetButton ("Jump") && isJumping) {
+            if (jumpTimeCounter > 0) {
+                ApplyJumpForce ();
                 jumpTimeCounter -= Time.deltaTime;
-            }
-            else
-            {
+            } else {
                 isJumping = false;
             }
         }
 
-        if (Input.GetButtonUp("Jump"))
-        {
+        if (Input.GetButtonUp ("Jump")) {
             isJumping = false;
         }
 
+        //WallSliding
+        isTouchingWall = Physics2D.OverlapCircle (wallCheck.position, checkRadius, groundLayers);
+        if (isTouchingWall && !isGrounded && horizontalAxisInput != 0)
+            isWallSliding = true;
+        else
+            isWallSliding = false;
+
+        if (isWallSliding) {
+            playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, Mathf.Clamp(playerRigidbody2D.velocity.y, -wallSlidingSpeed.getValue(), float.MaxValue));
+            // playerRigidbody2D.gravityScale = playerRigidbody2D.velocity.y > 0 ? gravityScaleUp - wallSlidingSpeed.getValue() : gravityScaleDown - wallSlidingSpeed.getValue();
+        }
+
         //Dashing
-        if (playerState == State.Idle || playerState == State.Jumping)
-        {
-            if (Input.GetButtonDown("Dash") && dashCooldown < Time.time)
-            {
-                playerAudioSource.PlayOneShot(dashSound, dashSoundVolume);
+        if (playerState == State.Idle || playerState == State.Jumping) {
+            if (Input.GetButtonDown ("Dash") && dashCooldown < Time.time) {
+                playerAudioSource.PlayOneShot (dashSound, dashSoundVolume);
                 playerState = State.Dashing;
                 dashDirecton = isFacingRight ? 1 : -1;
-                dashCooldown = Time.time + startDashCooldown.getValue();
+                dashCooldown = Time.time + startDashCooldown.getValue ();
             }
             // if (Input.GetButtonDown("Attack"))     // Attacking in this script? One script for all actions?  MD
             // {
             //     // playerState = State.Attacking;
             // }
-        }
-        else if (playerState == State.Dashing)
-        {
-            if (dashTime <= 0)
-            {
+        } else if (playerState == State.Dashing) {
+            if (dashTime <= 0) {
                 dashDirecton = 0;
-                if (isGrounded)         // making new dash available when grounded, also possible with cooldown  MD
+                if (isGrounded) // making new dash available when grounded, also possible with cooldown  MD
                 {
                     playerState = State.Idle;
                     dashTime = startDashTime;
                 }
-            }
-            else
-            {
+            } else {
                 dashTime -= Time.deltaTime;
-                playerRigidbody2D.velocity = new Vector2(dashDirecton, 0) * dashSpeed.getValue();
+                playerRigidbody2D.velocity = new Vector2 (dashDirecton, 0) * dashSpeed.getValue ();
             }
         }
 
         //Flip character sprite based on move direction
         //TODO Statt dem axisinput lieber die rigibody-velocity verwenden? BH
-        if (horizontalAxisInput > 0 && !isFacingRight)
-        {
-            flip();
-        }
-        else if (horizontalAxisInput < 0 && isFacingRight)
-        {
-            flip();
+        if (horizontalAxisInput > 0 && !isFacingRight) {
+            flip ();
+        } else if (horizontalAxisInput < 0 && isFacingRight) {
+            flip ();
         }
     }
 
-    public void ApplyJumpForce()
-    {
-        playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, jumpForce.getValue());
+    public void ApplyJumpForce () {
+        playerRigidbody2D.velocity = new Vector2 (playerRigidbody2D.velocity.x, jumpForce.getValue ());
     }
 
-    void OnDrawGizmos()
-    {
+    public void ApplyJumpForce (float _xValue) {
+        playerRigidbody2D.velocity = new Vector2 (_xValue, jumpForce.getValue ());
+    }
+
+    void OnDrawGizmos () {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(groundCheckTransform.position, groundCheckRadius);
+        Gizmos.DrawWireSphere (groundCheckTransform.position, checkRadius);
     }
 
-    public bool isAirborne()
-    {
+    public bool isAirborne () {
         return !isGrounded;
     }
 
-    public void setGroundSoundCollection(GroundSoundCollection newCollection)
-    {
-        if (currentGroundSoundCollection != newCollection)
-        {
+    public void setGroundSoundCollection (GroundSoundCollection newCollection) {
+        if (currentGroundSoundCollection != newCollection) {
             this.currentGroundSoundCollection = newCollection;
-            Debug.Log("Ground Sound Collection switched to: " + newCollection.name);
+            Debug.Log ("Ground Sound Collection switched to: " + newCollection.name);
         }
     }
 }

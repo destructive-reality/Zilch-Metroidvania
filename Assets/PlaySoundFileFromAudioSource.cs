e68c3ec82b8d52f0de46ae57537f6163613ec02b
Bastian Hodapp <bastianhodapp@gmail.com>
2020-12-28 14:25:42 +0100

    Trying out spine events
    now working!

---
+++ Assets/PlaySoundFileFromAudioSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundFileFromAudioSource : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void playAudioClipFromAudioSource(AudioClip audioClip)
+    {
+        if (audioClip)
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+}

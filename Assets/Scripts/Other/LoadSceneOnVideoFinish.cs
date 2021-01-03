ba96ca6ad64a9751866b6e39df4d8db99128cfe8
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-03 12:22:20 +0100

    Add LoadSceneOnVideoFinish script

---
+++ Assets/Scripts/Other/LoadSceneOnVideoFinish.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class LoadSceneOnVideoFinish : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public int levelToLoad;
+
+    void Start()
+    {
+        if (!videoPlayer)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+        else
+        {
+            Debug.LogError("No VideoPlayer found");
+        }
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}

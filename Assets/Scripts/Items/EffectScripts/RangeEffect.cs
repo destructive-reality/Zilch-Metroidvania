3a43148274c6c2de368f27aba073c1a52f5d3b62
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-01-03 16:56:37 +0100

    Add RangeAttack-Effect to Range-Modifier

--- Assets/Scripts/Items/EffectScripts/RangeEffect.cs
+++ Assets/Scripts/Items/EffectScripts/RangeEffect.cs
@@ -2,8 +2,14 @@
 
 public class RangeEffect : Effect
 {
+  PlayerMovement playerMovement;
   private float attackRangeBoost = 0.3f;
   private float dashTimeBoost = 0.2f;
+  private PlayerCombat2D playerCombat;
+  public Transform projectilePrefab;
+  private float rangeAttackCooldown = 1f;
+  private float rangeAttackTimer = 0;
+
   public override void ArmStart(bool value = true)
   {
 
@@ -11,7 +17,7 @@ public class RangeEffect : Effect
   public override void LegStart(bool value = true)
   {
     Debug.Log("Increase Player Dash Time: " + value);
-    PlayerMovement playerMovement = gameObject.GetComponentInParent<PlayerMovement>();
+    playerMovement = gameObject.GetComponentInParent<PlayerMovement>();
     if (value)
       playerMovement.startDashTime += dashTimeBoost;
     else
@@ -20,7 +26,7 @@ public class RangeEffect : Effect
   public override void WeaponStart(bool value = true)
   {
     Debug.Log("Increase Player Attack Range: " + value);
-    PlayerCombat2D playerCombat = gameObject.GetComponentInParent<PlayerCombat2D>();
+    playerCombat = gameObject.GetComponentInParent<PlayerCombat2D>();
     if (value)
       playerCombat.attackRange.addModifier(attackRangeBoost);
     else
@@ -28,6 +34,9 @@ public class RangeEffect : Effect
   }
   public override void HeadStart(bool value = true)
   {
+    Debug.Log("Enable Range Attack: " + value);
+    playerCombat = gameObject.GetComponentInParent<PlayerCombat2D>();
+    playerMovement = gameObject.GetComponentInParent<PlayerMovement>();
 
   }
   public override void ArmUpdate()
@@ -44,6 +53,20 @@ public class RangeEffect : Effect
   }
   public override void HeadUpdate()
   {
+    if (Time.time >= rangeAttackTimer && Input.GetButtonDown("Shoot"))
+    {
+      Debug.Log("Shoot");
+      rangeAttackTimer = Time.time + rangeAttackCooldown;
+      FireProjectile();
+    }
+  }
+  private void FireProjectile()
+  {
+    Vector2 dir = Vector2.right;
+    dir.x = playerMovement.isFacingRight ? 1 : -1;
 
+    Vector2 origin = playerCombat.attackPoint.position;
+    Transform tsfProjectile = Instantiate(projectilePrefab, origin, Quaternion.identity);
+    tsfProjectile.GetComponent<Projectile>().Setup(dir, true, 0.7f);
   }
 }

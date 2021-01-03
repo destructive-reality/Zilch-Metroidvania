9fc49ec6d0a9fae1cf74d7ec8cca286c41049aa0
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-01-03 11:45:49 +0100

    fix #390 and let Doof move on the ground

--- Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/Movement Behaviors/PlatformMovementBehaviour.cs
+++ Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/Movement Behaviors/PlatformMovementBehaviour.cs
@@ -1,36 +1,38 @@
 using UnityEngine;
 
-public class PlatformMovementBehaviour : StateMachineBehaviour {
-    protected HorizontalMovingEnemy enemyScript;
-    protected float speed;
+public class PlatformMovementBehaviour : StateMachineBehaviour
+{
+  protected HorizontalMovingEnemy enemyScript;
+  protected float speed;
 
-    #region MovementOnPlatform
-    protected Transform groundDetector;
-    protected float rayDistance = 1f;
-    protected LayerMask moveOnLayer;
-    #endregion
+  #region MovementOnPlatform
+  protected Transform groundDetector;
+  protected float rayDistance = 1f;
+  protected LayerMask moveOnLayer;
+  #endregion
 
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        enemyScript = animator.GetComponent<HorizontalMovingEnemy>();
-        speed = enemyScript.speed.getValue();
-        moveOnLayer = LayerMask.GetMask("Ground");
-        groundDetector = enemyScript.groundDetector;
-    }
+  public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+  {
+    enemyScript = animator.GetComponent<HorizontalMovingEnemy>();
+    speed = enemyScript.speed.getValue();
+    moveOnLayer = LayerMask.GetMask("Ground");
+    groundDetector = enemyScript.groundDetector;
+  }
 
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+  public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+  {
+    RaycastHit2D groundInfo = Physics2D.Raycast(groundDetector.position, Vector2.down, rayDistance, moveOnLayer);
+    if (groundInfo.collider == null || groundInfo.distance < 0.01f)
     {
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetector.position, Vector2.down, rayDistance, moveOnLayer);
-        if (groundInfo.collider == null)
-        {
-            return;
-        }
-        if (enemyScript.isFacingRight)
-        {
-            animator.transform.Translate(Vector2.right * speed * Time.deltaTime);
-        }
-        else
-        {
-            animator.transform.Translate(Vector2.left * speed * Time.deltaTime);
-        }
+      return;
     }
+    if (enemyScript.isFacingRight)
+    {
+      animator.transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+    else
+    {
+      animator.transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+  }
 }

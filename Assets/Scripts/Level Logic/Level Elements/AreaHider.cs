71f01f7b525bcece9899fdb81bf6bf692a306cf7
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-01-08 16:03:42 +0100

    Add fading to Area hider
    Außerdem könnte an den auch noch ein Sound angehangen werden. Sowas wie "Pling, du hast ein Secret entdeckt"

--- Assets/Scripts/Level Logic/Level Elements/AreaHider.cs
+++ Assets/Scripts/Level Logic/Level Elements/AreaHider.cs
@@ -1,15 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
 public class AreaHider : MonoBehaviour
 {
-  [SerializeField] Light2D hidingLight;
+  [SerializeField] private Light2D hidingLight;
+  [SerializeField] private float fadeDuration;
+  [SerializeField] private float targetIntensity;
+  // [SerializeField] private AudioSource audioSource;
 
   private void OnTriggerEnter2D(Collider2D other)
   {
     if (other.CompareTag("Player"))
     {
-      hidingLight.enabled = false;
+      // audioSource.PlayOneShot(audioSource.clip);
+      // hidingLight.enabled = false;
+      StartCoroutine("fadeLight");
+      gameObject.GetComponent<Collider2D>().enabled = false;
     }
   }
+
+  private IEnumerator fadeLight()
+  {
+    float currentTime = 0;
+
+    while (currentTime < fadeDuration)
+    {
+      currentTime += Time.deltaTime;
+      // hidingLight.volumeOpacity = Mathf.Lerp(hidingLight.volumeOpacity, 0, currentTime / fadeDuration);
+      hidingLight.intensity = Mathf.Lerp(hidingLight.intensity, targetIntensity, currentTime / fadeDuration);
+
+      yield return null;
+    }
+    hidingLight.enabled = false;
+    yield break;
+  }
+
 }

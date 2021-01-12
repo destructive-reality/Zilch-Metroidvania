21f30ed7e028eebfc173c17c52a9d731769fb061
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-12 11:09:16 +0100

    Create teleporter prefab

---
+++ Assets/Scripts/Level Logic/Level Elements/Teleporter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject teleportTarget;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = teleportTarget.transform.position;
+        }
+    }
+}

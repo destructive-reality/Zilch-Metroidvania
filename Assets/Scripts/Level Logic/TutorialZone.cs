0db13f3483b442cd2b44cb641e5bcece5796d625
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-14 15:03:32 +0100

    Add tutorial zone prefab

---
+++ Assets/Scripts/Level Logic/TutorialZone.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class TutorialZone : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.Universal.Light2D spotlightLight;
+    // public float lightFadeDuration;
+    private AudioSource audioSource;
+    private bool lightOn = false;
+
+    private bool soundAlreadyPlayed = false;
+
+    // private float startLightIntensity;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        // startLightIntensity = spotlightLight.intensity;
+        // spotlightLight.intensity = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            spotlightLight.enabled = true;
+            if (!soundAlreadyPlayed)
+            {
+                audioSource.Play();
+                soundAlreadyPlayed = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            spotlightLight.enabled = false;
+        }
+    }
+
+    // private void Update()
+    // {
+    //     if (lightOn)
+    //     {
+    //         spotlightLight.intensity = Mathf.Lerp(0, startLightIntensity, lightFadeDuration);
+    //     }
+    //     else
+    //     {
+    //         spotlightLight.intensity = Mathf.Lerp(startLightIntensity, 0, lightFadeDuration);
+    //     }
+    // }
+}

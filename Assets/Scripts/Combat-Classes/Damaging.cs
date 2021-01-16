ec4dc626725a1dc8c7b48d914157078231cb2c33
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-01-16 14:53:08 +0100

    prepare groundforce adjustment

--- Assets/Scripts/Combat-Classes/Damaging.cs
+++ Assets/Scripts/Combat-Classes/Damaging.cs
@@ -2,23 +2,23 @@
 
 public abstract class Damaging : MonoBehaviour
 {
-    public Animator animator;
-    public Stat attackPower;
-    public Stat attackRange;
+  // public Animator animator;
+  public Stat attackPower;
+  public Stat attackRange;
 
-    public static void DealDamage(GameObject _source, int _damage, GameObject _target)
+  public static void DealDamage(GameObject _source, int _damage, GameObject _target)
+  {
+    if (_target.GetComponent<PlayerHealth>())
     {
-        if (_target.GetComponent<PlayerHealth>())
-        {
-            _target.GetComponent<PlayerHealth>().getHit(_damage, _source);
-        }
-        else if (_target.GetComponent<KnockbackHealth>())
-        {
-            _target.GetComponent<KnockbackHealth>().getHit(_damage, _source);
-        }
-        else
-        {
-            _target.GetComponent<Health>().applyDamage(_damage);
-        }
+      _target.GetComponent<PlayerHealth>().getHit(_damage, _source);
     }
+    else if (_target.GetComponent<KnockbackHealth>())
+    {
+      _target.GetComponent<KnockbackHealth>().getHit(_damage, _source);
+    }
+    else
+    {
+      _target.GetComponent<Health>().applyDamage(_damage);
+    }
+  }
 }
--- Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/Attack Behaviors/GroundForceBehaviour.cs
+++ Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/Attack Behaviors/GroundForceBehaviour.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class GroundForceBehaviour : StateMachineBehaviour
 {
-    [SerializeField] private Transform prefabGameObject;
-    private BossEnemyAnimator animatorScript;
-    private BossCombat combatScript;
+  [SerializeField] private Transform prefabGameObject;
+  private BossEnemyAnimator animatorScript;
+  private BossCombat combatScript;
 
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animatorScript = animator.GetComponent<BossEnemyAnimator>();
-        combatScript = animator.GetComponent<BossCombat>();
+  public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+  {
+    animatorScript = animator.GetComponent<BossEnemyAnimator>();
+    combatScript = animator.GetComponent<BossCombat>();
 
-        Transform tsfGroundAttack = Instantiate(prefabGameObject, combatScript.rangeAttackPoint.position, Quaternion.identity);
-        tsfGroundAttack.GetComponent<GroundForce>().Setup(6, 1);
-    }
+    Transform tsfGroundAttack = Instantiate(prefabGameObject, combatScript.rangeAttackPoint.position, Quaternion.identity);
+    tsfGroundAttack.GetComponent<GroundForce>().Setup(6, 1);
+  }
 
-    // public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
-    // {
-    // }
+  // public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+  // {
+  // }
 
-    // public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
-    // {
-    // }
+  // public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+  // {
+  // }
 }

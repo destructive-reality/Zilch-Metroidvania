6381eee50b7b431d51829fb3d1785251af94fa14
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-01-19 19:56:00 +0100

    open equipment with inventory

--- Assets/Scripts/NonPlayer/Enemies/Items/Equipment/EquipmentUI.cs
+++ Assets/Scripts/NonPlayer/Enemies/Items/Equipment/EquipmentUI.cs
@@ -2,40 +2,40 @@ using UnityEngine;
 
 public class EquipmentUI : MonoBehaviour
 {
-    public Transform itemsParent;
-    private GameObject equipmentUIGO;
-    private Equipment equipment;
-    private EquipmentSlot[] slots;
+  public Transform itemsParent;
+  private GameObject equipmentUIGO;
+  private Equipment equipment;
+  private EquipmentSlot[] slots;
 
-    private void Start()
+  private void Start()
+  {
+    equipmentUIGO = itemsParent.gameObject;
+    equipment = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Equipment>();
+    /* equipment.changedEquipmentCallback += UpdateEquipmentUI; */
+
+    slots = itemsParent.GetComponentsInChildren<EquipmentSlot>();
+    for (int i = 0; i < slots.Length; i++)
     {
-        equipmentUIGO = itemsParent.gameObject;
-        equipment = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Equipment>();
-        /* equipment.changedEquipmentCallback += UpdateEquipmentUI; */
-
-        slots = itemsParent.GetComponentsInChildren<EquipmentSlot>();
-        for (int i = 0; i < slots.Length; i++)
-        {
-            slots[i].InitializeEquipmentSlotPosition(i);
-        }
+      slots[i].InitializeEquipmentSlotPosition(i);
     }
+  }
 
-    private void Update()
+  private void Update()
+  {
+    if (Input.GetButtonDown("Inventory") && !PauseMenu.GameIsPaused)
     {
-        if (Input.GetButtonDown("Equipment") && !PauseMenu.GameIsPaused)
-        {
-            SetEquipmentUIVisible(!equipmentUIGO.activeSelf);
-        }
+      SetEquipmentUIVisible(!equipmentUIGO.activeSelf);
     }
+  }
 
-    public void SetEquipmentUIVisible(bool visibleBoolean)
+  public void SetEquipmentUIVisible(bool visibleBoolean)
+  {
+    equipmentUIGO.SetActive(visibleBoolean);
+
+    //Hacky fix with mouse position, but works for now...
+    if (visibleBoolean == false && Input.mousePosition.x < Screen.width / 2)
     {
-        equipmentUIGO.SetActive(visibleBoolean);
-
-        //Hacky fix with mouse position, but works for now...
-        if (visibleBoolean == false && Input.mousePosition.x < Screen.width / 2)
-        {
-            TooltipHoverBoxController.Instance.setVisible(false);
-        }
+      TooltipHoverBoxController.Instance.setVisible(false);
     }
+  }
 }

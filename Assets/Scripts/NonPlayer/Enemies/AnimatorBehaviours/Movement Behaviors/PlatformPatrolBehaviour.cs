4ea6a6ce080f78f39698ae26560399df68ca9c31
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-21 10:45:19 +0100

    Kind of fix #440 - new sprite for patrolling enemy

--- Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/Movement Behaviors/PlatformPatrolBehaviour.cs
+++ Assets/Scripts/NonPlayer/Enemies/AnimatorBehaviours/Movement Behaviors/PlatformPatrolBehaviour.cs
@@ -2,41 +2,44 @@
 
 public class PlatformPatrolBehaviour : StateMachineBehaviour
 {
-  private float speed;
-  private Transform groundDetection;
-  private float rayDistance = 1f;
-  private LayerMask moveOnLayer;
-  private PlatformPatrollingEnemyAnimator enemyScript;
+    private float speed;
+    private Transform groundDetection;
+    private float rayDistance = 1f;
+    private LayerMask moveOnLayer;
+    private PlatformPatrollingEnemyAnimator enemyScript;
 
-  override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-  {
-    enemyScript = animator.GetComponent<PlatformPatrollingEnemyAnimator>();
-    speed = enemyScript.speed.getValue();
-    groundDetection = enemyScript.groundDetector;
+    private Transform parentTransform;
 
-    moveOnLayer = LayerMask.GetMask("Ground");
-
-  }
-
-  override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-  {
-    RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, rayDistance, moveOnLayer);
-    if (groundInfo.collider == null || groundInfo.distance < 0.1f)
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-      // Debug.Log ("PatrollingEnemy needs to flip");
-      enemyScript.flip();
-    }
-    if (enemyScript.isFacingRight)
-    {
-      animator.transform.Translate(Vector2.right * speed * Time.deltaTime);
-    }
-    else
-    {
-      animator.transform.Translate(Vector2.left * speed * Time.deltaTime);
-    }
-  }
+        parentTransform = animator.transform.parent;
+        enemyScript = parentTransform.GetComponent<PlatformPatrollingEnemyAnimator>();
+        speed = enemyScript.speed.getValue();
+        groundDetection = enemyScript.groundDetector;
 
-  // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-  //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-  //}
+        moveOnLayer = LayerMask.GetMask("Ground");
+
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, rayDistance, moveOnLayer);
+        if (groundInfo.collider == null || groundInfo.distance < 0.1f)
+        {
+            // Debug.Log ("PatrollingEnemy needs to flip");
+            enemyScript.flip();
+        }
+        if (enemyScript.isFacingRight)
+        {
+            parentTransform.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            parentTransform.transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //}
 }

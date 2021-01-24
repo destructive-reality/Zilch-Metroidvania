6b08e8c415557412d8611b138e604ab1aef34b8f
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-24 20:04:26 +0100

    Add camera shake when falling object hits the ground

---
+++ Assets/Scripts/Level Logic/Level Elements/CameraShakeOnTriggerEnter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeOnTriggerEnter : MonoBehaviour
+{
+    public float intensity = 3f;
+    public float duration = 1f;
+
+    public string tagName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(tagName))
+        {
+            CinemachineShake.Instance.ShakeCamera(intensity, duration);
+        }
+    }
+}

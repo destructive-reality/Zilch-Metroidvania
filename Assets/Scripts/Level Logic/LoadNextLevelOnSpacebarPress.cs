6c84b23976a15b0c3b5b86ba88b3dcb79c40fb41
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-27 16:44:11 +0100

    Add spacebar intro movie skip

---
+++ Assets/Scripts/Level Logic/LoadNextLevelOnSpacebarPress.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LoadNextLevelOnSpacebarPress : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LevelLoader.Instance.LoadNextLevel();
+        }
+    }
+}

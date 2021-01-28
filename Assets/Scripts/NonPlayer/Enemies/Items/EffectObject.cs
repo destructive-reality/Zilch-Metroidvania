58e0730d845e5425add786e6ff794c0fa03e0dd1
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-01-28 21:28:02 +0100

    Let modifiers display current slot effect

--- Assets/Scripts/NonPlayer/Enemies/Items/EffectObject.cs
+++ Assets/Scripts/NonPlayer/Enemies/Items/EffectObject.cs
@@ -3,8 +3,8 @@
 [CreateAssetMenu(fileName = "New EffectObject", menuName = "Assets/Effect Object")]
 public class EffectObject : ScriptableObject
 {
-    public string effectName;
-    // public MonoBehaviour effectScript;
-    public bool isUpdate;
-    public bool isStart;
+  public string effectName;
+  public bool isUpdate;
+  public bool isStart;
+  public string effectDiscription;
 }
--- Assets/Scripts/NonPlayer/Enemies/Items/Modifier.cs
+++ Assets/Scripts/NonPlayer/Enemies/Items/Modifier.cs
@@ -2,41 +2,40 @@
 
 public class Modifier : MonoBehaviour
 {
-    [SerializeField] private GameObject modifierEffect;
-    private bool isInteractable = false;
-    private Inventory playerInventory;
+  [SerializeField] private GameObject modifierEffect;
+  private bool isInteractable = false;
+  private Inventory playerInventory;
 
-    [Header("Sounds")]
-    public AudioClip pickupSound;
-    public float pickupSoundVolume = 1f;
+  [Header("Sounds")]
+  public AudioClip pickupSound;
+  public float pickupSoundVolume = 1f;
 
 
-    private void OnTriggerEnter2D(Collider2D other)
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.CompareTag("Player"))
     {
-        if (other.CompareTag("Player"))
-        {
-            isInteractable = true;
-            playerInventory = other.GetComponentInChildren<Inventory>();
-        }
+      isInteractable = true;
+      playerInventory = other.GetComponentInChildren<Inventory>();
     }
+  }
 
-    private void Update()
+  private void Update()
+  {
+    if (isInteractable && Input.GetButtonDown("PickUp") && playerInventory)
     {
-        if (isInteractable && Input.GetButtonDown("PickUp") && playerInventory)
-        {
-            Debug.Log(pickupSound);
-            AudioSource.PlayClipAtPoint(pickupSound, Camera.main.transform.position, pickupSoundVolume);
-            playerInventory.AddItem(modifierEffect);
-            modifierEffect.transform.parent = playerInventory.gameObject.transform;
-            Destroy(this.gameObject);
-        }
+      AudioSource.PlayClipAtPoint(pickupSound, Camera.main.transform.position, pickupSoundVolume);
+      playerInventory.AddItem(modifierEffect);
+      modifierEffect.transform.parent = playerInventory.gameObject.transform;
+      Destroy(this.gameObject);
     }
+  }
 
-    private void OnTriggerExit2D(Collider2D other)
+  private void OnTriggerExit2D(Collider2D other)
+  {
+    if (other.CompareTag("Player"))
     {
-        if (other.CompareTag("Player"))
-        {
-            isInteractable = false;
-        }
+      isInteractable = false;
     }
+  }
 }

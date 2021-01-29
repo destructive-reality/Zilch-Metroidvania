249bc0c6d1c405392466deef2d97c38028b5727e
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-29 20:54:45 +0100

    Add Audio trigger zone for phil

---
+++ Assets/Scripts/Level Logic/AudioTriggerZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(AudioSource))]
+public class AudioTriggerZone : MonoBehaviour
+{
+    public string triggeringTag;
+    public bool triggerOnlyOnce = true;
+    private bool hasAlreadyBeenTriggered = false;
+    private AudioSource triggerZoneAudioSource;
+
+    void Awake()
+    {
+        triggerZoneAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((!triggerOnlyOnce || !hasAlreadyBeenTriggered) && other.CompareTag(triggeringTag))
+        {
+            triggerZoneAudioSource.Play();
+            hasAlreadyBeenTriggered = true;
+        }
+    }
+}

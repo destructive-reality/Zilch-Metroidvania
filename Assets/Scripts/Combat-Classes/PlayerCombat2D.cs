9403ebd034b853ac9f5e2e92358de748197b7dda
Bastian Hodapp <bastianhodapp@gmail.com>
2021-01-29 20:46:21 +0100

    Zilch up and down attack animations

--- Assets/Scripts/Combat-Classes/PlayerCombat2D.cs
+++ Assets/Scripts/Combat-Classes/PlayerCombat2D.cs
@@ -1,65 +1,84 @@
 ﻿using UnityEngine;
 
+public enum ATTACK_DIRECTION
+{
+    UP, DOWN, START
+}
+
 public class PlayerCombat2D : Damaging
 {
-  public PlayerMasterController playerMasterController;
-  public Transform attackPoint;
-  public LayerMask attackableLayers;       // remeber assigning layers
-  public Stat attackTime;
-  public Animator animator;
-  protected float nextAttackTime = 0.3f;
+    public PlayerMasterController playerMasterController;
+    public Transform attackPoint;
+    public LayerMask attackableLayers;       // remeber assigning layers
+    public Stat attackTime;
+    public Animator playerAnimator;
+    protected float nextAttackTime = 0.3f;
+    public ATTACK_DIRECTION attackDirection = ATTACK_DIRECTION.START;
 
-  private void Update()
-  {
-    if (Time.time >= nextAttackTime)
+    private void Update()
     {
-      if (Input.GetButtonDown("Attack"))
-      {
-        startAttack();
-        // playerState = State.Attacking;
-        nextAttackTime = Time.time + attackTime.getValue();
-      }
-    }
-  }
-
-  public void startAttack()
-  {
-    animator.SetTrigger("Attack");
-    dealDamage();
-  }
-  private void dealDamage()
-  {
-    Collider2D[] hitTargets = Physics2D.OverlapCircleAll(attackPoint.position, attackRange.getValue(), attackableLayers);
-
-    //If anything was hit, do some screen shake
-    if (hitTargets.Length > 0)
-    {
-      CinemachineShake.Instance.ShakeCamera(2.5f, .3f);
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetButtonDown("Attack"))
+            {
+                startAttack();
+                // playerState = State.Attacking;
+                nextAttackTime = Time.time + attackTime.getValue();
+            }
+        }
     }
 
-    // remember to give enemies 2D-Colliders        MD
-    foreach (Collider2D target in hitTargets)
+    public void startAttack()
     {
-      if (target.isTrigger)
-      {
-        continue;
-      }
-
-      int targetLayer = target.gameObject.layer;
-      if (targetLayer == LayerMask.NameToLayer("Destroyable") || targetLayer == LayerMask.NameToLayer("Attackable"))
-      {
-        // Debug.Log("Hit Destroyable");
-        target.GetComponent<Health>().applyDamage((int)attackPower.getValue());
-      }
-      else if (targetLayer == LayerMask.NameToLayer("Enemy"))
-        target.GetComponent<KnockbackHealth>().getHit((int)attackPower.getValue(), transform.position); //Change KnockbackHealth back to EnemyHealth?
+        switch (attackDirection)
+        {
+            case ATTACK_DIRECTION.UP:
+                playerAnimator.SetTrigger("Attack Up");
+                break;
+            case ATTACK_DIRECTION.DOWN:
+                playerAnimator.SetTrigger("Attack Down");
+                break;
+            case ATTACK_DIRECTION.START:
+                playerAnimator.SetTrigger("Attack Normal");
+                break;
+            default:
+                break;
+        }
+        dealDamage();
     }
-  }
+    private void dealDamage()
+    {
+        Collider2D[] hitTargets = Physics2D.OverlapCircleAll(attackPoint.position, attackRange.getValue(), attackableLayers);
 
-  private void OnDrawGizmosSelected()
-  {
-    if (attackPoint == null)
-      return;
-    Gizmos.DrawWireSphere(attackPoint.position, attackRange.getValue());
-  }
+        //If anything was hit, do some screen shake
+        if (hitTargets.Length > 0)
+        {
+            CinemachineShake.Instance.ShakeCamera(2.5f, .3f);
+        }
+
+        // remember to give enemies 2D-Colliders        MD
+        foreach (Collider2D target in hitTargets)
+        {
+            if (target.isTrigger)
+            {
+                continue;
+            }
+
+            int targetLayer = target.gameObject.layer;
+            if (targetLayer == LayerMask.NameToLayer("Destroyable") || targetLayer == LayerMask.NameToLayer("Attackable"))
+            {
+                // Debug.Log("Hit Destroyable");
+                target.GetComponent<Health>().applyDamage((int)attackPower.getValue());
+            }
+            else if (targetLayer == LayerMask.NameToLayer("Enemy"))
+                target.GetComponent<KnockbackHealth>().getHit((int)attackPower.getValue(), transform.position); //Change KnockbackHealth back to EnemyHealth?
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange.getValue());
+    }
 }

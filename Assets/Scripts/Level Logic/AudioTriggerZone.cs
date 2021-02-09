95d32e25135c7e81a58c0907b0599e4d01e07198
Bastian Hodapp <bastianhodapp@gmail.com>
2021-02-09 14:28:16 +0100

    Fix #477
    Audio trigger zones now have  a toggle to cancel all other audio trigger zones from playing when they're triggered

--- Assets/Scripts/Level Logic/AudioTriggerZone.cs
+++ Assets/Scripts/Level Logic/AudioTriggerZone.cs
@@ -7,7 +7,12 @@ using UnityEngine;
 public class AudioTriggerZone : MonoBehaviour
 {
     public string triggeringTag;
+
+    [Tooltip("Only trigger the first time an object with the triggering tag enters this zone?")]
     public bool triggerOnlyOnce = true;
+
+    [Tooltip("Should all other AudioTriggerZones be stopped when this one is triggered?")]
+    public bool stopAllOtherAudioTriggerZones = true;
     private bool hasAlreadyBeenTriggered = false;
     private AudioSource triggerZoneAudioSource;
 
@@ -20,8 +25,17 @@ public class AudioTriggerZone : MonoBehaviour
     {
         if ((!triggerOnlyOnce || !hasAlreadyBeenTriggered) && other.CompareTag(triggeringTag))
         {
+            if (stopAllOtherAudioTriggerZones)
+            {
+                foreach (AudioTriggerZone otherAudioTriggerZone in FindObjectsOfType<AudioTriggerZone>())
+                {
+                    otherAudioTriggerZone.triggerZoneAudioSource.Stop();
+                }
+            }
             triggerZoneAudioSource.Play();
             hasAlreadyBeenTriggered = true;
+
+
         }
     }
 }

200cbdfa3aa7664bd4ee6709fc37cfcdf40de170
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-02-09 16:13:49 +0100

    Add AreaHider which use sprites
    Funktionieren besser als AreaHider, die Licht nutzen

---
+++ Assets/Scripts/Level Logic/Level Elements/SpriteAreaHider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAreaHider : MonoBehaviour
+{
+  [SerializeField] private SpriteRenderer hiderSprite;
+  [SerializeField] private float alphaReduction = 0.02f;
+  // [SerializeField] private AudioSource audioSource;
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.CompareTag("Player"))
+    {
+      // audioSource.PlayOneShot(audioSource.clip);
+      StartCoroutine("fadeHider");
+      gameObject.GetComponent<Collider2D>().enabled = false;
+    }
+  }
+
+  private IEnumerator fadeHider()
+  {
+    Color workingColor = hiderSprite.color;
+
+    while (hiderSprite.color.a >= 0)
+    {
+      workingColor.a -= alphaReduction;
+      hiderSprite.color = workingColor;
+      yield return null;
+    }
+
+    Destroy(gameObject);
+    yield break;
+  }
+}

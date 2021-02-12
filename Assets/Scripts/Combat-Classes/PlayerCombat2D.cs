e0d2e83f8b86f62aa2a694aadac8208c3f1a7f4b
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-02-12 19:53:25 +0100

    add Particles when hitting something

--- Assets/Scripts/Combat-Classes/PlayerCombat2D.cs
+++ Assets/Scripts/Combat-Classes/PlayerCombat2D.cs
@@ -2,96 +2,97 @@
 
 public enum ATTACK_DIRECTION
 {
-    UP, DOWN, START
+  UP, DOWN, START
 }
 
 public class PlayerCombat2D : Damaging
 {
-    public PlayerMasterController playerMasterController;
-    public Transform attackPoint;
-    public LayerMask attackableLayers;       // remeber assigning layers
-    public Stat attackTime;
-    public Animator playerAnimator;
-    protected float nextAttackTime = 0.3f;
-    public ATTACK_DIRECTION attackDirection = ATTACK_DIRECTION.START;
+  public PlayerMasterController playerMasterController;
+  public Transform attackPoint;
+  [SerializeField] private ParticleSystem attackParticles;
+  public LayerMask attackableLayers;       // remeber assigning layers
+  public Stat attackTime;
+  public Animator playerAnimator;
+  protected float nextAttackTime = 0.3f;
+  public ATTACK_DIRECTION attackDirection = ATTACK_DIRECTION.START;
 
-    [Header("Sounds")]
-    public AudioClip attackSound;
-    private AudioSource playerAudioSource;
+  [Header("Sounds")]
+  public AudioClip attackSound;
+  private AudioSource playerAudioSource;
 
-    private void Awake()
+  private void Awake()
+  {
+    playerAudioSource = GetComponent<AudioSource>();
+  }
+
+  private void Update()
+  {
+    if (Time.time >= nextAttackTime)
     {
-        playerAudioSource = GetComponent<AudioSource>();
+      if (Input.GetButtonDown("Attack"))
+      {
+        startAttack();
+        // playerState = State.Attacking;
+        nextAttackTime = Time.time + attackTime.getValue();
+      }
+    }
+  }
+
+  public void startAttack()
+  {
+    switch (attackDirection)
+    {
+      case ATTACK_DIRECTION.UP:
+        playerAnimator.SetTrigger("Attack Up");
+        break;
+      case ATTACK_DIRECTION.DOWN:
+        playerAnimator.SetTrigger("Attack Down");
+        break;
+      case ATTACK_DIRECTION.START:
+        playerAnimator.SetTrigger("Attack Normal");
+        break;
+      default:
+        break;
+    }
+    dealDamage();
+    if (playerAudioSource && attackSound)
+    {
+      playerAudioSource.PlayOneShot(attackSound);
+    }
+  }
+  private void dealDamage()
+  {
+    Collider2D[] hitTargets = Physics2D.OverlapCircleAll(attackPoint.position, attackRange.getValue(), attackableLayers);
+
+    //If anything was hit, do some screen shake
+    if (hitTargets.Length > 0)
+    {
+      CinemachineShake.Instance.ShakeCamera(2.5f, .3f);
+      attackParticles.Play();
     }
 
-    private void Update()
+    foreach (Collider2D target in hitTargets)
     {
-        if (Time.time >= nextAttackTime)
-        {
-            if (Input.GetButtonDown("Attack"))
-            {
-                startAttack();
-                // playerState = State.Attacking;
-                nextAttackTime = Time.time + attackTime.getValue();
-            }
-        }
+      if (target.isTrigger)
+      {
+        continue;
+      }
+
+      int targetLayer = target.gameObject.layer;
+      if (targetLayer == LayerMask.NameToLayer("Destroyable") || targetLayer == LayerMask.NameToLayer("Attackable"))
+      {
+        // Debug.Log("Hit Destroyable");
+        target.GetComponent<Health>().applyDamage((int)attackPower.getValue());
+      }
+      else if (targetLayer == LayerMask.NameToLayer("Enemy"))
+        target.GetComponent<KnockbackHealth>().getHit((int)attackPower.getValue(), transform.position); //Change KnockbackHealth back to EnemyHealth?
     }
+  }
 
-    public void startAttack()
-    {
-        switch (attackDirection)
-        {
-            case ATTACK_DIRECTION.UP:
-                playerAnimator.SetTrigger("Attack Up");
-                break;
-            case ATTACK_DIRECTION.DOWN:
-                playerAnimator.SetTrigger("Attack Down");
-                break;
-            case ATTACK_DIRECTION.START:
-                playerAnimator.SetTrigger("Attack Normal");
-                break;
-            default:
-                break;
-        }
-        dealDamage();
-        if (playerAudioSource && attackSound)
-        {
-            playerAudioSource.PlayOneShot(attackSound);
-        }
-    }
-    private void dealDamage()
-    {
-        Collider2D[] hitTargets = Physics2D.OverlapCircleAll(attackPoint.position, attackRange.getValue(), attackableLayers);
-
-        //If anything was hit, do some screen shake
-        if (hitTargets.Length > 0)
-        {
-            CinemachineShake.Instance.ShakeCamera(2.5f, .3f);
-        }
-
-        // remember to give enemies 2D-Colliders        MD
-        foreach (Collider2D target in hitTargets)
-        {
-            if (target.isTrigger)
-            {
-                continue;
-            }
-
-            int targetLayer = target.gameObject.layer;
-            if (targetLayer == LayerMask.NameToLayer("Destroyable") || targetLayer == LayerMask.NameToLayer("Attackable"))
-            {
-                // Debug.Log("Hit Destroyable");
-                target.GetComponent<Health>().applyDamage((int)attackPower.getValue());
-            }
-            else if (targetLayer == LayerMask.NameToLayer("Enemy"))
-                target.GetComponent<KnockbackHealth>().getHit((int)attackPower.getValue(), transform.position); //Change KnockbackHealth back to EnemyHealth?
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        if (attackPoint == null)
-            return;
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange.getValue());
-    }
+  private void OnDrawGizmosSelected()
+  {
+    if (attackPoint == null)
+      return;
+    Gizmos.DrawWireSphere(attackPoint.position, attackRange.getValue());
+  }
 }

111b86b789b686f259b06144d053567b7815acb6
Bastian Hodapp <bastianhodapp@gmail.com>
2021-02-16 18:44:48 +0100

    UI Sound fix?

--- Assets/Scripts/UI Behaviors/PlayUISoundOnStateEnterBehavior.cs
+++ Assets/Scripts/UI Behaviors/PlayUISoundOnStateEnterBehavior.cs
@@ -6,12 +6,20 @@ public class PlayUISoundOnStateEnterBehavior : StateMachineBehaviour
 {
     public AudioClip soundToPlay;
 
-    [Range(0.0f, 1.0f)]
+    [Range(0.0f, 4.0f)]
     public float audioVolume = 1f;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        AudioSource.PlayClipAtPoint(soundToPlay, Camera.main.transform.position, audioVolume);
+        if (UIController.Instance)
+        {
+            UIController.Instance.getUiAudioSource().PlayOneShot(soundToPlay, audioVolume);
+        }
+        else
+        {
+            AudioSource.PlayClipAtPoint(soundToPlay, Camera.main.transform.position, audioVolume);
+        }
+
     }
 
 }

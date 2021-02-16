1bbc8fc7e341a1bd84233abc1d507089937f2abd
MarkusDamm <markus.damm@hs-furtwangen.de>
2021-02-16 12:23:49 +0100

    seemingly fix #431

--- Assets/Scripts/Level Logic/AudioZone.cs
+++ Assets/Scripts/Level Logic/AudioZone.cs
@@ -5,75 +5,81 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class AudioZone : MonoBehaviour
 {
-    private AudioSource audioSource;
-    private float initialVolume;
-    private IEnumerator currentCoroutine;
-    [SerializeField] private Light2D globalLight;
-    [SerializeField] private float lightAdjustment = 0.5f;
+  private AudioSource audioSource;
+  private float initialVolume;
+  private IEnumerator currentCoroutine;
+  [SerializeField] private Light2D globalLight;
+  [SerializeField] private float lightAdjustment = 0.5f;
+  [SerializeField] private float fadeDuration;
 
-    public float fadeDuration;
+  void Awake()
+  {
+    audioSource = GetComponent<AudioSource>();
+    initialVolume = audioSource.volume;
+    audioSource.volume = 0f;
+  }
 
-    void Awake()
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.CompareTag("Player"))
     {
-        audioSource = GetComponent<AudioSource>();
-        initialVolume = audioSource.volume;
-        audioSource.volume = 0f;
+      audioSource.pitch = Random.Range(0.95f, 1.05f);
+      //   audioSource.time = Random.Range(0f, audioSource.clip.length);
+      if (!audioSource.isPlaying)
+      {
+        audioSource.time = 0f;
+      }
+
+      fadeTo(initialVolume, lightAdjustment);
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D other)
+  {
+    if (other.CompareTag("Player"))
+    {
+      fadeTo();
+    }
+  }
+
+  private void fadeTo(float _volume = 0, float _lightIntensity = 0)
+  {
+    if (currentCoroutine != null)
+    {
+      StopCoroutine(currentCoroutine);
+    }
+    if (_lightIntensity == 0)
+      currentCoroutine = startFade(fadeDuration, _volume);
+    else
+      currentCoroutine = startFade(fadeDuration, _volume, _lightIntensity);
+    StartCoroutine(currentCoroutine);
+  }
+
+  private IEnumerator startFade(float _duration, float _targetVolume = 0, float _targetLightIntensity = 0)
+  {
+    float currentTime = 0;
+    float start = audioSource.volume;
+
+    if (_targetVolume != 0 && !audioSource.isPlaying)
+    {
+      audioSource.Play();
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    while (currentTime < _duration)
     {
-        if (other.CompareTag("Player"))
-        {
-            audioSource.pitch = Random.Range(0.95f, 1.05f);
-            audioSource.time = Random.Range(0f, audioSource.clip.length);
-
-            fadeTo(initialVolume, lightAdjustment);
-        }
+      currentTime += Time.deltaTime;
+      audioSource.volume = Mathf.Lerp(start, _targetVolume, currentTime / _duration);
+      if (_targetVolume <= 0f && audioSource.volume <= 0f)
+      {
+        audioSource.Stop();
+        Debug.Log("Stopping Audio source from audiozone");
+      }
+      if (_targetLightIntensity > 0)
+      {
+        globalLight.intensity = Mathf.Lerp(globalLight.intensity, _targetLightIntensity, currentTime / _duration);
+      }
+      yield return null;
     }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            fadeTo(0f);
-        }
-    }
-
-    private void fadeTo(float volume, float _lightIntensity = 0)
-    {
-        if (currentCoroutine != null)
-        {
-            StopCoroutine(currentCoroutine);
-        }
-        if (_lightIntensity == 0)
-            currentCoroutine = startFade(fadeDuration, volume);
-        else
-            currentCoroutine = startFade(fadeDuration, volume, _lightIntensity);
-        StartCoroutine(currentCoroutine);
-    }
-
-    private IEnumerator startFade(float duration, float targetVolume, float _targetLightIntensity = 0)
-    {
-        float currentTime = 0;
-        float start = audioSource.volume;
-
-        audioSource.Play();
-
-        while (currentTime < duration)
-        {
-            currentTime += Time.deltaTime;
-            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
-            if (targetVolume <= 0f && audioSource.volume <= 0f)
-            {
-                audioSource.Stop();
-                Debug.Log("Stopping Audio source from audiozone");
-            }
-            if (_targetLightIntensity > 0)
-            {
-                globalLight.intensity = Mathf.Lerp(globalLight.intensity, _targetLightIntensity, currentTime / duration);
-            }
-            yield return null;
-        }
-        yield break;
-    }
+    yield break;
+  }
 }

c624c3fe9da73bbc87bcedc06aa76b25585f2a4e
Bastian Hodapp <bastianhodapp@gmail.com>
2021-02-16 12:00:43 +0100

    Fixes #490 - Zilch damage sound

--- Assets/Scripts/Health-Classes/PlayerHealth.cs
+++ Assets/Scripts/Health-Classes/PlayerHealth.cs
@@ -2,43 +2,55 @@ using UnityEngine;
 
 public class PlayerHealth : KnockbackHealth
 {
-  public PlayerMasterController playerMasterController;
-  public Stat startInvincibleTime;
-  private float invincibleTime = 0f;
-  public int lowLifePulseThreshold = 1;
+    public PlayerMasterController playerMasterController;
+    public Stat startInvincibleTime;
+    private float invincibleTime = 0f;
+    public int lowLifePulseThreshold = 1;
 
-  public override void setHealth(int healthToSetTo)
-  {
-    base.setHealth(healthToSetTo);
-    UIController.Instance.setHealthInUI(currentHealth, (int)maxHealth.getValue());
-    PostProController.Instance.SetLowLifePulseBool(currentHealth <= lowLifePulseThreshold ? true : false);
-  }
+    [Header("Sounds")]
+    public AudioClip takeDamageAudioClip;
+    private AudioSource playerAudioSource;
+    [Range(0f, 4f)]
+    public float takeDamageAudioClipVolume = 1f;
 
-  public new void getHit(int damage, Vector2 damagingObject)
-  {
-    if (Time.time >= invincibleTime)
+    private void Awake()
     {
-      base.getHit(damage, damagingObject);
-      // applyKnockback(new Vector2(this.transform.position.x < damagingObject.x ? -knockbackForce.getValue() : knockbackForce.getValue(), knockbackForce.getValue() / 4));
-      startInvincibilty();
-      PostProController.Instance.TriggerChromaticAberrationDamageAnimation();
-      CinemachineShake.Instance.ShakeCamera(2f, .1f);
-      GameMaster.Instance.SlowdownTime(.6f, 1.5f);
+        playerAudioSource = GetComponent<AudioSource>();
     }
-  }
 
-  public void startInvincibilty()
-  {
-    invincibleTime = Time.time + startInvincibleTime.getValue();
-  }
+    public override void setHealth(int healthToSetTo)
+    {
+        base.setHealth(healthToSetTo);
+        UIController.Instance.setHealthInUI(currentHealth, (int)maxHealth.getValue());
+        PostProController.Instance.SetLowLifePulseBool(currentHealth <= lowLifePulseThreshold ? true : false);
+    }
 
-  protected override void die()
-  {
-    base.die();
-    LevelLoader.Instance.ReloadCurrentLevel();
-    GetComponent<PlayerMovement>().enabled = false;
-    GetComponent<PlayerCombat2D>().enabled = false;
-    playerMasterController.playerMovementController.playerAnimator.SetTrigger("Death");
-    Debug.Log(gameObject.name + " is dying. Bye!");
-  }
+    public new void getHit(int damage, Vector2 damagingObject)
+    {
+        if (Time.time >= invincibleTime)
+        {
+            base.getHit(damage, damagingObject);
+            // applyKnockback(new Vector2(this.transform.position.x < damagingObject.x ? -knockbackForce.getValue() : knockbackForce.getValue(), knockbackForce.getValue() / 4));
+            startInvincibilty();
+            PostProController.Instance.TriggerChromaticAberrationDamageAnimation();
+            CinemachineShake.Instance.ShakeCamera(2f, .1f);
+            GameMaster.Instance.SlowdownTime(.6f, 1.5f);
+            playerAudioSource.PlayOneShot(takeDamageAudioClip, takeDamageAudioClipVolume);
+        }
+    }
+
+    public void startInvincibilty()
+    {
+        invincibleTime = Time.time + startInvincibleTime.getValue();
+    }
+
+    protected override void die()
+    {
+        base.die();
+        LevelLoader.Instance.ReloadCurrentLevel();
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<PlayerCombat2D>().enabled = false;
+        playerMasterController.playerMovementController.playerAnimator.SetTrigger("Death");
+        Debug.Log(gameObject.name + " is dying. Bye!");
+    }
 }
